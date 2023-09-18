dcf5b10bc370ffdfbf192522bd5899dcf7ba4e61
elpapuuXD <eltomascenturion@gmail.com>
2023-09-18 14:36:19 -0300

    creación de servicio Data

--- src/app/pages/login/registro/registro.component.ts
+++ src/app/pages/login/registro/registro.component.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Validators } from '@angular/forms';
+import { DataService } from 'src/app/data.service';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
   styleUrls: ['./registro.component.css']
 })
-export class RegistroComponent {
+export class RegistroComponent implements OnInit {
 
-  get User(){
-    
-    return 
-  }
+
+
+constructor (datos: DataService){}
+
+ngOnInit(): void {}
+
+onRegis(): void {
+
+  alert("");
+}  
 
 }
+
+
